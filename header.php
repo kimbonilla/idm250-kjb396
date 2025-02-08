9ef658d9615869b137529219ffd7e8c75c8a1080
kimbonilla <147077374+kimbonilla@users.noreply.github.com>
2025-02-07 23:49:58 -0500

    single page template

--- header.php
+++ header.php
@@ -9,3 +9,22 @@
     </title>
     <?php wp_head(); ?>
 </head>
+<header>
+    <section class="top-nav">
+        <div>
+            <a href="index.html"><img src="" alt="Logo"></a>
+        </div>
+        <input id="menu-toggle" type="checkbox">
+        <label class="menu-button-container" for="menu-toggle">
+        <span class="menu-button"></span>
+        </label>
+        <ul class="menu">
+            <li><a href="">About Us</a></li>
+            <li><a href="">E-Board</a></li>
+            <li><a href="">Barrio Fiesta</a></li>
+            <li><a href="">Gallery</a></li>
+            <li><a href="">Calendar</a></li>
+            <li><a href="">Contact Us</a></li>
+        </ul>
+    </section>
+</header>
